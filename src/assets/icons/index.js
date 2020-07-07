21499492e75f07e6c310d3d951ed56ff482a91d7
Lorenzo <lopescernicchiaro@gmail.com>
2020-07-07 19:04:54 -0300

    added icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,1 +1,3 @@
-export {default as BackgroundFruits} from './backgroundFruits.svg';
+export { default as BackgroundFruits } from './backgroundFruits.svg';
+export { default as Hamburger } from './Hamburger.svg';
+export { default as AddUser } from './AddUser.svg';
